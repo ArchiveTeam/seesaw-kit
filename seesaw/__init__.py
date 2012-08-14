18e06e7d048f0cb7ec0585ac2d15355990a602e1
Alard <alard@example.com>
2012-08-14 10:44:14 +0200

    Version 0.0.3.

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [ 'config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'task', 'tracker', 'warrior', 'web' ]
 
 __title__ = 'seesaw'
-__version__ = '0.0.2'
-__build__ = 0x00002
+__version__ = '0.0.3'
+__build__ = 0x00003
 
