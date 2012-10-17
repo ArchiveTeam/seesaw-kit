4865a136954c2fdfde90d46cc0e6694271ba2eca
Alard <alard@example.com>
2012-10-17 11:20:23 +0200

    Version 0.0.10, not 0.0.1.

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -1,7 +1,7 @@
 __all__ = [ 'config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'task', 'tracker', 'warrior', 'web' ]
 
 __title__ = 'seesaw'
-__version__ = '0.0.1'
+__version__ = '0.0.10'
 __build__ = 0x00010
 runner_type = ""
 
