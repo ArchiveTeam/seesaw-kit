d353ed19970e4c829b947f6e7238482cf4dfab0d
Christopher Foo <chris.foo@gmail.com>
2013-11-14 14:59:38 +0800

    Version 0.0.16.

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'util', 'task', 'tracker', 'warrior', 'web']
 
 __title__ = 'seesaw'
-__version__ = '0.0.15'
-__build__ = 0x00015
+__version__ = '0.0.16'
+__build__ = 0x00016
 runner_type = ""
 warrior_build = ""
