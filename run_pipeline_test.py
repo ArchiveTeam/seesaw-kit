4335208cde5b0f13da0ce0f8054a2a4a22fc181f
Christopher Foo <chris.foo@gmail.com>
2013-11-15 22:24:07 +0800

    Prepares for Python 3 support.
    Squashed commit of the following:
    
    commit c1e47a8cdeef52a22b08bc9cd1e15c6bb8852ae5
    Author: Christopher Foo <chris.foo@gmail.com>
    Date:   Fri Nov 15 22:19:04 2013 +0800
    
        Remove python 3 travis testing. Fix version_info check.
    
    commit b666f294601eff7755a8b6be784b763c186f4a0d
    Author: Christopher Foo <chris.foo@gmail.com>
    Date:   Fri Nov 15 22:02:11 2013 +0800
    
        Fix travis config
    
    commit ff7059d36962b8a0d5e83d21d23a1d01d5049f50
    Author: Christopher Foo <chris.foo@gmail.com>
    Date:   Fri Nov 15 21:58:54 2013 +0800
    
        Configs Travis to execute 2to3

--- run_pipeline_test.py
+++ run_pipeline_test.py
@@ -1,10 +1,17 @@
 import unittest
 import subprocess
+import sys
 
 
 class RunPipelineTest(unittest.TestCase):
     def test_example_pipeline(self):
+        if sys.version_info[0] == 3:
+            python_exe = 'python3'
+        else:
+            python_exe = 'python'
+
         subprocess.check_call([
+            python_exe,
             './run-pipeline',
             'example-pipeline.py',
             'testuser',
