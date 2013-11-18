ee3b72ff16c95d175927e6659beb6a8a09b86831
Christopher Foo <chris.foo@gmail.com>
2013-11-18 21:13:12 +0800

    Version 0.1.1
    Using semantic versioning. 0.0.16 is considered to be 0.1.0.

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'util', 'task', 'tracker', 'warrior', 'web']
 
 __title__ = 'seesaw'
-__version__ = '0.1.1b1'
+__version__ = '0.1.1'
 __build__ = 0x00101
 runner_type = ""
 warrior_build = ""
