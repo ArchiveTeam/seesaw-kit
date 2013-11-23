7b9cfad39fb800b0f7d60b8cd8b570255b3cf308
Christopher Foo <chris.foo@gmail.com>
2013-11-23 13:02:34 +0800

    Version 0.1.3

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -2,7 +2,7 @@
 __all__ = ['config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'util', 'task', 'tracker', 'warrior', 'web']
 
 __title__ = 'seesaw'
-__version__ = '0.1.2'
-__build__ = 0x00102
+__version__ = '0.1.3'
+__build__ = 0x00103
 runner_type = ""
 warrior_build = ""
