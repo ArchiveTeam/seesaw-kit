92fa4d6ff8d064ca1fdd84aba9620f229bbe4701
Christopher Foo <chris.foo@gmail.com>
2013-12-08 12:38:41 +0800

    Bump version to 0.1.4b1.

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -2,7 +2,7 @@
 __all__ = ['config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'util', 'task', 'tracker', 'warrior', 'web']
 
 __title__ = 'seesaw'
-__version__ = '0.1.3b1'
-__build__ = 0x00103
+__version__ = '0.1.4b1'
+__build__ = 0x00104
 runner_type = ""
 warrior_build = ""
