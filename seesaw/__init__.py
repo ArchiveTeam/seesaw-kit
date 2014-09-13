4c771e1ffaa6061b1bb274a98fb110ca9ce32bb6
Christopher Foo <chris.foo@gmail.com>
2014-09-13 12:04:22 -0400

    Bump version 0.7.0b1

--- seesaw/__init__.py
+++ seesaw/__init__.py
@@ -1,7 +1,7 @@
 '''ArchiveTeam seesaw kit'''
 
 __title__ = 'seesaw'
-__version__ = '0.6.0b1'
-__build__ = 0x00600
+__version__ = '0.7.0b1'
+__build__ = 0x00700
 runner_type = ""
 warrior_build = ""
