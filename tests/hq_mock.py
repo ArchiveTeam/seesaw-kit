ff5ad2a4c6e33048707e046110f8b0ebcd9ee2a6
Christopher Foo <chris.foo@gmail.com>
2014-12-10 12:26:17 -0500

    hq_mock: Fix indents and add a local project

--- tests/hq_mock.py
+++ tests/hq_mock.py
@@ -19,29 +19,46 @@ class UpdateHandler(tornado.web.RedirectHandler):
     def post(self):
         self.write({
             "warrior": {
-              "seesaw_version": "0.0.15"
-           },
-           "broadcast_message": "<i>Hello world</i>",
-           "auto_project": "testproject",
-           "projects": [
-              {
-                 "name": "testproject",
-                 "title": "A test project",
-                 "description": "Testing a project",
-                 "repository":
-                    "https://github.com/ArchiveTeam/example-seesaw-project",
-                 "logo":
-                    "https://raw.github.com/ArchiveTeam/warrior-preseed/master"
-                    "/splash/Archive_team-white.png",
-                 "marker_html": "hi",
-                 "lat_lng": [
-                    0.0,
-                    0.0
-                 ],
-                 "leaderboard": "http://example.com/"
-              },
-           ]
-       })
+                "seesaw_version": "0.0.15"
+            },
+            "broadcast_message": "<i>Hello world</i>",
+            "auto_project": "testproject",
+            "projects": [
+                {
+                    "name": "testproject",
+                    "title": "A test project",
+                    "description": "Testing a project",
+                    "repository":
+                        "https://github.com/ArchiveTeam/example-seesaw-project",
+                    "logo":
+                        "https://raw.github.com/ArchiveTeam/warrior-preseed/master"
+                        "/splash/Archive_team-white.png",
+                    "marker_html": "hi",
+                    "lat_lng": [
+                        0.0,
+                        0.0
+                    ],
+                    "leaderboard": "http://example.com/"
+                },
+                {
+                    "name": "localproject",
+                    "title": "A local project",
+                    "description":
+                        "A project loaded from /tmp/mywarriorproject "
+                        "Useful for testing auto update project",
+                    "repository":
+                        "/tmp/mywarriorproject",
+                    "logo":
+                        "",
+                    "marker_html": "hi",
+                    "lat_lng": [
+                        0.0,
+                        0.0
+                    ],
+                    "leaderboard": "http://example.com/"
+                },
+            ]
+        })
 
 
 if __name__ == '__main__':
